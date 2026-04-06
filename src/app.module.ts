3f77c472cb2c3a954d60afbcf5621effd6a8613f
Kimthina Ly <kimthinaly7@gmail.com>
2026-04-06 14:37:52 +0700

    Initial commit: Baby Essentials Store with NestJS and ConfigModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,18 +10,34 @@ import { AddressModule } from './address/address.module';
 import { AuthModule } from './auth/auth.module';
 import { CartModule } from './cart/cart.module';
 import { OrdersModule } from './orders/orders.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'root',
-    password: '01685027',
-    database: 'babyessential',
-    autoLoadEntities: true,
-    synchronize: false,
-  }), ProductsModule, CategoriesModule, OccasionsModule, UserModule, AddressModule, AuthModule, CartModule, OrdersModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USER'),
+        password: configService.get<string>('DB_PASS'),
+        database: configService.get<string>('DB_NAME'),
+        autoLoadEntities: true,
+        synchronize: false,
+      }),
+    }),
+    ProductsModule,
+    CategoriesModule,
+    OccasionsModule,
+    UserModule,
+    AddressModule,
+    AuthModule,
+    CartModule,
+    OrdersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
